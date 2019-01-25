acfa7ae729dba4148a1df28702e045d2d8fd1653
Jung Bin Park <jbp2522@tamu.edu>
2019-01-24 20:12:17 -0600

    changed sh

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
 
+sudo xbps-install -Syuv
 cat packages.txt | xargs sudo xbps-install -Sy
+
+cp ./config/Xresources ~/.Xresources
+cp ./config/bash_profile ~/.bash_profile
+cp ./config/xinitrc ~/.xinitrc
+mkdir -p ~/.config/i3/
+cp ./config/i3config ~/.config/i3/config
+cp ./config/i3status ~/.i3status.conf
